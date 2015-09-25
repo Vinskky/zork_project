99e518cc2ffb48714035599f2471889704c42486
Vinskky <naitsabesdv@gmail.com>
2015-09-25 11:50:45 +0100

    zork

---
+++ Zork/Zork/testingRooms.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+
+struct Room
+{
+	char* mesage = "welcome to zork game, enjoy and have fun";
+	char* comand1 = "Quit";
+};
+
+
+int main()
+{
+	Room test;
+	bool play = true;
+	while (play)
+	{
+
+	}
+	getchar();
+	return 0;
+}
+
