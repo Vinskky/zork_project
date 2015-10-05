8f4dbc8e9bb79d70714afcf8693a1748ab766a02
Vinskky <naitsabesdv@gmail.com>
2015-10-05 07:05:35 +0200

    changes

--- Zork/Zork/testingRooms.cpp
+++ Zork/Zork/testingRooms.cpp
@@ -1,23 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 #include <string.h>
 
-struct Room
+using namespace std;
+
+class Room
 {
-	char* mesage = "welcome to zork game, enjoy and have fun";
-	char* comand1 = "Quit";
+public:
+	char* intro = "Welcome to the world of zork a huge adventure is waiting for you, breace yourself and go! \n";
+	char* name = "now you are at the ";
+	char* north = NULL;
+	char* east = NULL;
+	char* west = NULL;
+	char* south = NULL;
+private:
 };
 
-
-int main()
+class Cabin :public class Room
 {
-	Room test;
-	bool play = true;
-	while (play)
+public:
+	char* intro = "You're inside this cabin its comfortable inside but there's nothing to eat maybe its time to hunt \n";
+	char* north = "At the north of the cabin you can see a pitfire and the head of a pig looking at you \n";
+	char* east = NULL;
+	char* west = "there's a door that conects with the world at west of the cabin \n";
+	char* south = "at south you have a window \n";
+	char* window = "All what you can see is darkness and an uncomfortable ambient \n";
+	void Look()
 	{
-
+		cout << intro << north << east << west << south << endl;
 	}
-	getchar();
-	return 0;
+private:
+};
+
+int main(int argc, char* argv)
+{
+	bool end = false;
+	Room forest;
+	Cabin cabin;
+	int x = 2, y = 3;
+	char* map[4][4] = 
+	{ "Forest1", "Forest2", "Forest3", "Forest4" 
+	"Forest5", "PoisonLake", "Forest6", "Forest7"
+	"Forest8", "Cabin", "Forest9", "Bigtree"
+	"Forest10", "Ritual Place","Forest11", "Forest12"
+	}
+	cabin.look();
+	cout << "what its your plan now? \n";
+	while (!end)
+	{
+		string comand;
+		geline(cin, comand);
+		if (comand == "window" && x == 2 && y == 3)
+		{
+			cout << cabin.window;
+		}
+		else if (comand == "window" && x == 2 && y == 3)
+	}
 }
 
